60766bfc20ae1d08cbacbed3af3e01aa9aa9d881
Baxter <baxter@gray.geek.nz>
2019-02-05 13:26:16 +1300

    added logging instead of the print command

---
+++ log_import.py
@@ -0,0 +1,11 @@
+"""
+imports and configures the logging
+"""
+import logging
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+# debug
+# info
+# warning
+# error
+# critical
